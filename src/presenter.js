d45f4b6cb095936dd9b7410df66fcce1e610bdc6
Fernando Rodriguez <fernando.ale.fars@gmail.com>
2024-08-19 13:19:15 -0400

    Funcionalidad Saludo por Nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,19 @@
 import saludar from "./saludador.js";
 
 const button = document.querySelector("button");
+const botonSaludoPorNombre = document.querySelector("#saludo-personalizado");
+const inputNombre = document.querySelector("#nombre");
 
 button.addEventListener("click", () => {
   const saludo = saludar();
   alert(saludo); // Muestra el saludo en una alerta
+});
+
+botonSaludoPorNombre.addEventListener("click", () => {
+  const nombre = inputNombre.value.trim();
+  if (nombre) {
+    alert(`Hola, ${nombre}!`);
+  } else {
+    alert("Por favor, escribe tu nombre.");
+  }
 });
