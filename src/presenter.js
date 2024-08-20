96c37de4cce292ced1e8a3e8c5578d3840372745
Fernando Rodriguez <fernando.ale.fars@gmail.com>
2024-08-20 00:09:42 -0400

    Saludo en Espaniol

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,11 @@
 import saludar from "./saludador.js";
 
 const button = document.querySelector("button");
-const botonSaludoPorEdad =document.querySelector("#saludo-edad");
+const botonSaludoEnEsapaniol =document.querySelector("#saludo-espaniol");
 const inputNombre = document.querySelector("#nombre");
 const inputGenero =document.querySelector("#genero");
 const inputEdad=document.querySelector("#edad");
+const selectIdioma = document.querySelector("#idioma");
 
 
 button.addEventListener("click", () => {
@@ -12,15 +13,16 @@ button.addEventListener("click", () => {
   alert(saludo); // Muestra el saludo en una alerta
 });
 
-
-
-botonSaludoPorEdad.addEventListener("click", ()=>{
+botonSaludoEnEsapaniol.addEventListener("click", ()=>{
   const genero=inputGenero.value.trim();
   const nombre=inputNombre.value.trim();
   const edad =inputEdad.value.trim();
+  const idioma=selectIdioma.value;
   if (nombre && genero && edad) {
-    alert(`Hola, ${nombre}!, género: ${genero}, edad: ${edad}`);
+    if (idioma === "es") {
+      alert(`Hola, ${nombre}!, género: ${genero}, edad: ${edad}`);
   } else {
     alert("Por favor, ingrese su nombre, género y edad.");
   }
+}
 })
