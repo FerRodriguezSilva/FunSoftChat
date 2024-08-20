92247f2cf53a4e5e02b752d8a92419bbf37dee83
Fernando Rodriguez <fernando.ale.fars@gmail.com>
2024-08-20 00:13:29 -0400

    Funcionalidad saludar en ingles

--- src/presenter.js
+++ src/presenter.js
@@ -1,17 +1,11 @@
 import saludar from "./saludador.js";
 
-const button = document.querySelector("button");
 const botonSaludoEnEsapaniol =document.querySelector("#saludo-espaniol");
 const inputNombre = document.querySelector("#nombre");
 const inputGenero =document.querySelector("#genero");
 const inputEdad=document.querySelector("#edad");
 const selectIdioma = document.querySelector("#idioma");
 
-
-button.addEventListener("click", () => {
-  const saludo = saludar();
-  alert(saludo); // Muestra el saludo en una alerta
-});
 
 botonSaludoEnEsapaniol.addEventListener("click", ()=>{
   const genero=inputGenero.value.trim();
@@ -21,8 +15,14 @@ botonSaludoEnEsapaniol.addEventListener("click", ()=>{
   if (nombre && genero && edad) {
     if (idioma === "es") {
       alert(`Hola, ${nombre}!, género: ${genero}, edad: ${edad}`);
+    } else if (idioma === "en") {
+      alert(`Hello, ${nombre}!, gender: ${genero}, age: ${edad}`);
+    }
   } else {
-    alert("Por favor, ingrese su nombre, género y edad.");
+    if (idioma === "es") {
+      alert("Por favor, ingrese su nombre, género y edad.");
+    } else if (idioma === "en") {
+      alert("Please enter your name, gender, and age.");
+    }
   }
-}
 })
